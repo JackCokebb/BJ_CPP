3ee448328c116559de4e4c17c958799c10b22faf
JackCokebb <reo2726@gmail.com>
2021-08-29 18:06:50 +0900

    no.10869

---
+++ 0x02/10869.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int x;
+    int y;
+    cin >>x >>y;
+    
+    cout << x+y << "\n";
+    cout << x-y << "\n";
+    cout << x*y << "\n";
+    cout << x/y << "\n";
+    cout << x%y << "\n";
+    return 0;
+}
