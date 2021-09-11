8e4c7b1efa055938f63d577cbd08ab9e50ab5fe6
JackCokebb <reo2726@gmail.com>
2021-09-11 22:47:52 +0900

    optimized

---
+++ 0x04/5397.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string input;
+    char comm, arg;
+    int commNum=0;
+    cin >> input;
+    cin >> commNum;
+
+    list<char> txt ={};
+    list<char>::iterator it = txt.begin();
+    
+
+    for(int i =0 ; i < (int)input.length(); i++){
+        txt.push_back(input[i]);
+        it++;
+    }
+    it++;
+
+    for(int i = 0; i< commNum; i++){
+        cin >> comm;
+        switch(comm){
+            case 'P' : 
+                cin >> arg;
+                txt.insert(it,arg);                  
+                break;
+            case 'L' : 
+                if(it == txt.begin())
+                    break;
+                else
+                    it--;
+                break;
+            case 'D' :
+                if(it== txt.end())
+                    break;
+                else{
+                    it++;
+                    break;
+                }
+            case 'B' : 
+                if(it != txt.begin()){
+                    it--;
+                    it = txt.erase(it);                 
+                }
+        }
+    }
+    for(auto t : txt){
+        cout << t;
+    }
+
+    return 0;
+}
